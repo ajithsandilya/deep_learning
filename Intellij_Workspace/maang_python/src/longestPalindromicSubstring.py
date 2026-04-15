1c61a1aaf02c5b9df5dfa88299886e790ddc06f4
ajithsandilya <ajithsandilyaapparaju@gmail.com>
2026-04-15 08:35:52 +0530

    longestPalindromicSubstring.py

--- Intellij_Workspace/maang_python/src/longestPalindromicSubstring.py
+++ Intellij_Workspace/maang_python/src/longestPalindromicSubstring.py
@@ -1,18 +1,32 @@
-def palindromeList(string):
-    palindromeLists = []
-    for i in range(len(string)):
-        for j in range(i, len(string)):
-            substring = string[i:j+1]
-            if substring == substring[::-1]:
-                palindromeLists.append(substring)
-    print(palindromeLists)
-    return palindromeLists
+#Brute Force Solution to find the longest palindromic substring in a given string
 
+def palindromeList(a):
+    palinlist=[]
+    for i in range(len(a)):
+        for j in range(i+1, len(a)+1):
+            if a[i:j] == a[i:j][::-1]:
+                palinlist.append(a[i:j])
+    return palinlist
+
+def longestPalindrome(palinlist):
+    longest = ""
+    longestlist = []
+    for i in palinlist:
+        if len(i) > len(longest):
+            longest = i
+    for i in palinlist:
+        if len(i) == len(longest):
+            longestlist.append(i)
+    return longestlist
 
 def main():
     string = input("Enter the string:" )
     print(string)
-    print(palindromeList(string))
+    palinlist = palindromeList(string)
+    print(palinlist)
+    print("Longest Substring is:")
+    print(longestPalindrome(palinlist))
+
 
 
 
